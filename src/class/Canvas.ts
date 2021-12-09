6856cb08860375c3ab4bb143138ba5c452143625
0bject / ts5h <at4@jc4.so-net.ne.jp>
2021-12-09 22:00:02 +0100

    Add Canvas class

---
+++ src/class/Canvas.ts
@@ -0,0 +1,40 @@
+export default class Canvas {
+  private wrapper: HTMLDivElement;
+
+  private canvas: HTMLCanvasElement;
+
+  private readonly ctx: CanvasRenderingContext2D | null;
+
+  constructor(elementID: string) {
+    this.canvas = document.getElementById(elementID) as HTMLCanvasElement;
+    this.wrapper = this.canvas.closest('div') as HTMLDivElement;
+    this.ctx = null;
+
+    this.initCanvas();
+  }
+
+  initCanvas(): void {
+    this.resizeCanvas();
+  }
+
+  resizeCanvas(): void {
+    const ww = this.wrapper.offsetWidth.toString();
+    const wh = this.wrapper.offsetHeight.toString();
+    this.canvas.setAttribute('width', ww);
+    this.canvas.setAttribute('height', wh);
+    this.canvas.style.width = `${ww}px`;
+    this.canvas.style.height = `${wh}px`;
+  }
+
+  getWindowWidth(): number {
+    return this.canvas.offsetWidth;
+  }
+
+  getWindowHeight(): number {
+    return this.canvas.offsetHeight;
+  }
+
+  // getContext() {
+  //   return <CanvasRenderingContext2D>this.ctx;
+  // }
+}
